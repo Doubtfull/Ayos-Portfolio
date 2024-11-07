2e5dcb1a9b38e06917f71c0ee10df8c87a3774ab
Doubtfull <myjoyishere13@gmail.com>
2024-11-07 14:19:25 -0500

    changed pfp and adjusted project card container

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/kirbysleeps.png")}
+        src={getImageUrl("hero/Sleeping-Kirby.gif")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
